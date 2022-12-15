5b2bc97791fa01831676e84f97264317e926c47e
CuriousKaiju <90697455+CuriousKaiju@users.noreply.github.com>
2022-12-15 14:38:47 +0100

    Cat navigation Fix

--- Assets/Scripts/Player Navigation/Player.cs
+++ Assets/Scripts/Player Navigation/Player.cs
@@ -2,29 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private PlayerInteraction _playerInteraction;
     [SerializeField] private NavigationAroundObject _navigationAroundObject;
-    private bool _externalNavigation;
+    private bool _externalNavigation = true;
 
     public void SetControllStatus(bool status)
     {
-        _playerInteraction.enabled = status;
-        _navigationAroundObject.enabled = status;
-        _navigationAroundObject.SetPitchAndYaw();
+        _externalNavigation = status;
+
+        if(_externalNavigation)
+        {
+            _navigationAroundObject.SetPitchAndYaw();
+        }
     }
 
-   
+
 
     private void Update()
     {
-        if(_externalNavigation)
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!_externalNavigation)
+            {
+                _externalNavigation = true;
+                DOTween.KillAll();
+                _navigationAroundObject.SetPitchAndYaw();
+            }
+        }
+
+        if (_externalNavigation)
         {
             _playerInteraction.TouchHandler();
             _navigationAroundObject.TouchHandler();
-        }
+        }    
     }
 
 
